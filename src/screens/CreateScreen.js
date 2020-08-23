6d9404b94f9234868b4f2176270cfa1b56a58f4f
sagarbehal08 <vivakbehal@gmail.com>
2020-08-23 15:51:40 +0530

    Add Symbol on IndexScreen and CreateScreen is Created

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,12 @@
+import React , {useContext} from 'react';
+import {Text,View,StyleSheet} from 'react-native';
+import { Context } from '../context/BlogContext';
+
+const CreateScreen=({navigation})=>{
+    return <Text>Create</Text>
+}
+
+const styles=StyleSheet.create({});
+
+
+export default CreateScreen;
