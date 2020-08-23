45cdcd757cc5e82c3eaa8730d18adcff6ac81613
sagarbehal08 <vivakbehal@gmail.com>
2020-08-23 16:08:04 +0530

    Input Form And Styling is Create and Make

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,12 +1,36 @@
-import React , {useContext} from 'react';
-import {Text,View,StyleSheet} from 'react-native';
+import React , {useState} from 'react';
+import {Text,View,StyleSheet,TextInput, Button} from 'react-native';
 import { Context } from '../context/BlogContext';
 
 const CreateScreen=({navigation})=>{
-    return <Text>Create</Text>
+    const [title,setTitle]=useState('');
+    const [content,setContent]=useState('');
+    return (<View>
+        <Text style={styles.label}>Enter Title:</Text>
+        <TextInput style={styles.input} value={title} onChangeText={(text)=>setTitle(text)}/>
+        <Text style={styles.label}>Enter Content:</Text>
+        <TextInput style={styles.input} value={content} onChangeText={(text)=>setContent(text)}/>
+        <Button title="Add Blog Post"/>
+    </View> 
+    );
 }
 
-const styles=StyleSheet.create({});
+const styles=StyleSheet.create({
+   input:{
+       fontSize:18,
+       borderWidth:1,
+       borderColor:'black',
+       padding:5,
+       margin:5,
+       marginBottom:15
+
+   },
+    label:{
+        fontSize:20,
+        marginBottom:5,
+        marginLeft:5
+    }
+});
 
 
 export default CreateScreen;
