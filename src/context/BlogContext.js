2d99ac4b746081bca579283bc68bfbedc501c54c
sagarbehal08 <vivakbehal@gmail.com>
2020-08-24 21:02:52 +0530

    Remove add_blogPost from reducer cases

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -13,14 +13,14 @@ const blogReducer=(state,action)=>{
             })
         case 'delete_blogPost':
             return state.filter((blogPost)=>blogPost.id!==action.payload);
-        case 'add_blogPost': 
-                return [...state,
-                    {
-                        id:Math.floor(Math.random()*99999),
-                        title:action.payload.title,
-                        content:action.payload.content
-                    }
-                ];
+        // case 'add_blogPost': 
+        //         return [...state,
+        //             {
+        //                 id:Math.floor(Math.random()*99999),
+        //                 title:action.payload.title,
+        //                 content:action.payload.content
+        //             }
+        //         ];
         default:
             return state;
     }
